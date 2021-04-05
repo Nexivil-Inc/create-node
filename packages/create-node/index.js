54322b27678047c97ac746ed76b2f36029f2f7e6
kdb621 <kdb621@nexivil.com>
2021-04-05 23:20:22 +0900

    create-node 0.0.1

---
+++ packages/create-node/index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+/**
+ * origin: Copyright (c) 2015-present, Facebook, Inc.
+ * modder: Copyright (c) 2021, Nexivil, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//   /!\ DO NOT MODIFY THIS FILE /!\
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// This file must work on Node 0.10+.
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//   /!\ DO NOT MODIFY THIS FILE /!\
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"use strict";
+
+var currentNodeVersion = process.versions.node;
+var semver = currentNodeVersion.split(".");
+var major = semver[0];
+
+if (major < 10) {
+  console.error(
+    "You are running Node " +
+      currentNodeVersion +
+      ".\n" +
+      "Create X Node requires Node 10 or higher. \n" +
+      "Please update your version of Node."
+  );
+  process.exit(1);
+}
+
+const { init } = require("./createNode");
+
+init();
