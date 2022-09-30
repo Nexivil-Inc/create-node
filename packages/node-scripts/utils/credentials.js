b3caa9bd3fd462ae4b12ca2fc8ad7ba677fe1141
kdb621 <liradb2000@live.co.kr>
2022-09-30 15:38:21 +0900

    Update Credential Manager;

--- packages/node-scripts/utils/credentials.js
+++ packages/node-scripts/utils/credentials.js
@@ -1,33 +1,96 @@
-"use strict";
+'use strict';
 
-const keytar = require("keytar");
-const chalk = require("chalk");
-const readline = require("readline");
+const keytar = require('keytar');
+const chalk = require('chalk');
+const readline = require('readline');
 
-async function getAuth(user) {
-    let token = await keytar.getPassword("nexivil/design-express", user);
-    if (token === null) {
-        console.info(`${chalk.blueBright("Please login Email : ")}'${user}'`);
-        token = await new Promise(r => {
-            let rl = readline.createInterface({
-                input: process.stdin,
-                output: process.stdout,
-            });
+const service = 'nexivil/design-express/v1';
 
-            rl.stdoutMuted = true;
+async function setAuth() {
+  let user, token;
+  //   let token = await keytar.getPassword('nexivil/design-express', user);
+  if (!user) {
+    user = await new Promise(r => {
+      let rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
 
-            rl.question("Access Token: ", function (_token) {
-                rl.close();
-                r(_token);
-            });
+      //   rl.stdoutMuted = true;
 
-            rl._writeToOutput = function _writeToOutput(stringToWrite) {
-                if (rl.stdoutMuted) rl.output.write("*");
-                else rl.output.write(stringToWrite);
-            };
-        });
-        await keytar.setPassword("nexivil/design-express", user, token);
-    }
-    return token;
+      rl.question(chalk.blueBright('Email: '), function (_user) {
+        rl.close();
+        r(_user);
+      });
+
+      rl._writeToOutput = function _writeToOutput(stringToWrite) {
+        if (rl.stdoutMuted) rl.output.write('*');
+        else rl.output.write(stringToWrite);
+      };
+    });
+  }
+  if (!token) {
+    token = await new Promise(r => {
+      let rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      rl.stdoutMuted = true;
+
+      rl.question(chalk.blueBright('Access Token: '), function (_token) {
+        rl.close();
+        r(_token);
+      });
+
+      rl._writeToOutput = function _writeToOutput(stringToWrite) {
+        if (rl.stdoutMuted) rl.output.write('*');
+        else rl.output.write(stringToWrite);
+      };
+    });
+    await keytar.setPassword(service, user, token);
+  }
+  return;
 }
-module.exports = { getAuth };
+
+async function getAuth() {
+  let user, token;
+  let cred = await keytar.findCredentials(service);
+  if (cred.length > 1) {
+    user = await new Promise(r => {
+      let rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      rl.stdoutMuted = true;
+      console.log(chalk.blueBright('We found multiple saved accounts.\n'));
+      console.log(chalk.blueBright('Please select a specific account.\n'));
+      rl.question(chalk.blueBright('Email: '), function (_user) {
+        rl.close();
+        r(_user);
+      });
+
+      rl._writeToOutput = function _writeToOutput(stringToWrite) {
+        if (rl.stdoutMuted) rl.output.write('*');
+        else rl.output.write(stringToWrite);
+      };
+    });
+    token = await keytar.getPassword(service, user);
+
+    if (token === null) {
+      console.log(chalk.redBright(`Cannot accesss a ${user} Credential.\n`));
+      console.log(
+        chalk.blueBright('npx @design-express/node-scripts setUser\n')
+      );
+      return;
+    }
+  } else {
+    const { account, password } = cred[0];
+    user = account;
+    token = password;
+  }
+  //   console.log(cred);
+  return { user, token };
+}
+module.exports = { getAuth, setAuth };
