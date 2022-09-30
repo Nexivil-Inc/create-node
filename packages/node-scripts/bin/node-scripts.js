f8eedf21987e46b743dc6759a0729a5dffc23d5e
kdb621 <liradb2000@live.co.kr>
2022-09-30 15:45:13 +0900

    Add "setuser" bin script for add credential;

--- packages/node-scripts/bin/node-scripts.js
+++ packages/node-scripts/bin/node-scripts.js
@@ -7,58 +7,59 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-"use strict";
+'use strict';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
 // terminate the Node.js process with a non-zero exit code.
-process.on("unhandledRejection", err => {
-    throw err;
+process.on('unhandledRejection', err => {
+  throw err;
 });
 
-const spawn = require("cross-spawn");
+const spawn = require('cross-spawn');
 const args = process.argv.slice(2);
 
 const scriptIndex = args.findIndex(
-    x =>
-        x === "start" ||
-        x === "build" ||
-        x === "pack" ||
-        x === "publish" ||
-        x === "docs"
+  x =>
+    x === 'start' ||
+    x === 'build' ||
+    x === 'pack' ||
+    x === 'publish' ||
+    x === 'docs' ||
+    x === 'setUser'
 );
 const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
 
-if (["start", "build", "pack", "publish", "docs"].includes(script)) {
-    const result = spawn.sync(
-        process.execPath,
-        nodeArgs
-            .concat(require.resolve("../scripts/" + script))
-            .concat(args.slice(scriptIndex + 1)),
-        { stdio: "inherit" }
-    );
-    if (result.signal) {
-        if (result.signal === "SIGKILL") {
-            console.log(
-                "The build failed because the process exited too early. " +
-                    "This probably means the system ran out of memory or someone called " +
-                    "`kill -9` on the process."
-            );
-        } else if (result.signal === "SIGTERM") {
-            console.log(
-                "The build failed because the process exited too early. " +
-                    "Someone might have called `kill` or `killall`, or the system could " +
-                    "be shutting down."
-            );
-        }
-        process.exit(1);
+if (['start', 'build', 'pack', 'publish', 'docs', 'setUser'].includes(script)) {
+  const result = spawn.sync(
+    process.execPath,
+    nodeArgs
+      .concat(require.resolve('../scripts/' + script))
+      .concat(args.slice(scriptIndex + 1)),
+    { stdio: 'inherit' }
+  );
+  if (result.signal) {
+    if (result.signal === 'SIGKILL') {
+      console.log(
+        'The build failed because the process exited too early. ' +
+          'This probably means the system ran out of memory or someone called ' +
+          '`kill -9` on the process.'
+      );
+    } else if (result.signal === 'SIGTERM') {
+      console.log(
+        'The build failed because the process exited too early. ' +
+          'Someone might have called `kill` or `killall`, or the system could ' +
+          'be shutting down.'
+      );
     }
-    process.exit(result.status);
+    process.exit(1);
+  }
+  process.exit(result.status);
 } else {
-    console.log('Unknown script "' + script + '".');
-    console.log("Perhaps you need to update @design-express/node-scripts?");
-    // console.log(
-    //     "See: https://facebook.github.io/create-react-app/docs/updating-to-new-releases"
-    // );
+  console.log('Unknown script "' + script + '".');
+  console.log('Perhaps you need to update @design-express/node-scripts?');
+  // console.log(
+  //     "See: https://facebook.github.io/create-react-app/docs/updating-to-new-releases"
+  // );
 }
