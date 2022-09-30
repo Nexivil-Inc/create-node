9ca1b6057fd80a0e72a231c68663c2f320cb6e42
kdb621 <liradb2000@live.co.kr>
2022-09-30 15:44:53 +0900

    Apply new file format publisher;

--- packages/node-scripts/scripts/publish.js
+++ packages/node-scripts/scripts/publish.js
@@ -1,126 +1,128 @@
-"use strict";
+/* eslint-disable no-extra-boolean-cast */
+'use strict';
 
-process.on("unhandledRejection", err => {
-    throw err;
+process.on('unhandledRejection', err => {
+  throw err;
 });
 
-const path = require("path");
-const https = require("https");
-const chalk = require("chalk");
-const crypto = require("crypto");
-const { readFileSync, existsSync } = require("fs");
-const ZstdCodec = require("zstd-codec").ZstdCodec;
+const path = require('path');
+const https = require('https');
+const chalk = require('chalk');
+const crypto = require('crypto');
+const { readFileSync, existsSync } = require('fs');
+const ZstdCodec = require('@liradb2000/zstd-codec/lib/zstd-codec');
 
-const { builder } = require("./plugins/builder");
-const { packer } = require("./plugins/packer");
-const { objectToFormData } = require("../utils/obj2formData");
-const { getAuth } = require("../utils/credentials");
-const { appPath } = require("../config/paths");
-
+const { objectToFormData } = require('../utils/obj2formData');
+const { getAuth } = require('../utils/credentials');
+const paths = require('../config/paths');
+const isLocalhost = process.env.localhost?.trim().toLowerCase() === 'true';
 const emailRegx = new RegExp(
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 );
+const nameRegex = new RegExp(/^(?:@(?<scope>\w+)\/)?(?<name>.+)$/);
+const XNODE_MAGIC_NUMBER = Buffer.from([0x58, 0x4e, 0x30, 0x44, 0x65], 'hex');
 
-console.log("Creating an optimized production build...");
+// console.log("Creating an optimized production build...");
 
-builder()
-    .then(options => packer(options))
-    .then(async ([bin, info]) => {
-        // Publish to server(design-express)
-        const { name, version, author, readme = null } = info;
-        let credential = null;
-        let readmeBin = null;
+async function publisher() {
+  const packInfo = require(paths.appPackageJson);
+  // Publish to server(design-express)
+  const { name: packName, version, readme = null, ..._packInfo } = packInfo;
 
-        if (!!author) {
-            if (typeof author === "string" && emailRegx.test(author)) {
-                credential = { user: author, token: await getAuth(author) };
-            } else if (author.name && emailRegx.test(author.name)) {
-                credential = {
-                    user: author.name,
-                    token: await getAuth(author.name),
-                };
-            } else if (author.email && emailRegx.test(author.email)) {
-                credential = {
-                    user: author.email,
-                    token: await getAuth(author.email),
-                };
-            } else {
-                console.error(chalk.red(`Cannot found 'author.email'`));
-                return;
-            }
-        } else {
-            console.error(chalk.red(`Cannot found 'author'`));
-            return;
-        }
+  const { scope: _scope, name } = nameRegex.exec(packName)?.groups ?? {};
+  const { user, token } = await getAuth();
+  const scope = !!_scope ? _scope.replace('@', '') : user;
 
-        if (!!readme) {
-            const readmePath = path.resolve(appPath, readme);
-            if (existsSync(readmePath))
-                readmeBin = await new Promise(r => {
-                    ZstdCodec.run(zstd => {
-                        const streaming = new zstd.Streaming();
-                        const data = readFileSync(readmePath);
-                        r(streaming.compress(data, 6));
-                    });
-                });
-            delete info["readme"];
-        } else {
-            const readmePath = path.resolve(appPath, "readme.md");
-            if (existsSync(readmePath))
-                readmeBin = await new Promise(r => {
-                    ZstdCodec.run(zstd => {
-                        const streaming = new zstd.Streaming();
-                        const data = readFileSync(readmePath);
-                        r(streaming.compress(data, 6));
-                    });
-                });
-        }
+  if (!name || !user || !token) {
+    chalk.redBright('something wrong.');
+    return;
+  }
 
-        const form = new objectToFormData(info, {
-            booleansAsIntegers: true,
+  const bin = readFileSync(
+    path.join(paths.appPath, `${name}-${version}.xnode`)
+  );
+  const magicNumber = bin.subarray(0, 5);
+  if (!XNODE_MAGIC_NUMBER.equals(magicNumber)) throw new Error('not xnode');
+  let readmeBin = null;
+
+  if (!!readme) {
+    const readmePath = path.resolve(paths.appPath, readme);
+    if (existsSync(readmePath))
+      readmeBin = await new Promise(r => {
+        ZstdCodec.run(zstd => {
+          const streaming = new zstd.Streaming();
+          const data = readFileSync(readmePath);
+          r(streaming.compress(data, 6));
         });
-
-        let integrity = crypto.createHash("sha1").update(bin).digest("hex");
-
-        form.append("integrity", integrity);
-        form.append("read_me", Buffer.from(readmeBin), {
-            filename: `${name}-${version}-readme.md`,
-            contentType: "application/octet-stream",
-            knownLength: readmeBin.length,
+      });
+    delete packInfo['readme'];
+  } else {
+    const readmePath = path.resolve(paths.appPath, 'readme.md');
+    if (existsSync(readmePath))
+      readmeBin = await new Promise(r => {
+        ZstdCodec.run(zstd => {
+          const streaming = new zstd.Streaming();
+          const data = readFileSync(readmePath);
+          r(streaming.compress(data, 6));
         });
-        form.append("tgz_file", Buffer.from(bin), {
-            filename: `${name}-${version}.xnode`,
-            contentType: "nexivil/xnode",
-            knownLength: bin.length,
-        });
+      });
+  }
 
-        const httpsOptions = {
-            method: "POST",
-            hostname: "x.nexivil.com",
-            port: 443,
-            path: "/-/package/",
-            headers: {
-                ...form.getHeaders(),
-                "Content-Length": form.getLengthSync(),
-                Authorization: `Basic ${Buffer.from(
-                    credential.user + ":" + credential.token,
-                    "utf8"
-                ).toString("base64")}`,
-            },
-        };
+  const form = new objectToFormData(_packInfo, {
+    booleansAsIntegers: true,
+  });
 
-        const req = https.request(httpsOptions, res => {
-            console.log(`statusCode: ${res.statusCode}`);
+  let integrity = crypto.createHash('sha1').update(bin).digest('hex');
 
-            res.on("data", d => {
-                process.stdout.write(d);
-            });
-        });
-
-        req.on("error", error => {
-            // console.error(error);
-        });
-        // form.pipe(req);
-        req.write(form.getBuffer());
-        req.end();
+  form.append('scope', scope);
+  form.append('name', name);
+  form.append('version', version);
+  form.append('integrity', integrity);
+  form.append('release_type', 'NODE');
+  form.append('x_version', '0');
+  if (!readmeBin && (readmeBin.length ?? 0) > 0) {
+    form.append('read_me', readmeBin, {
+      filename: `${name}-${version}-readme.md`,
+      contentType: 'application/octet-stream',
+      knownLength: readmeBin.length,
     });
+  }
+  form.append('tgz_file', bin, {
+    filename: `${name}-${version}.xnode`,
+    contentType: 'nexivil/xnode',
+    knownLength: bin.length,
+  });
+  // console.log(form);
+  const httpsOptions = {
+    method: 'POST',
+    hostname: isLocalhost ? 'localhost' : 'x.nexivil.com',
+    // hostname: 'x.nexivil.com',
+    port: isLocalhost ? 8005 : 443,
+    // port: 443,
+    path: '/-/package/',
+    headers: {
+      ...form.getHeaders(),
+      'Content-Length': form.getLengthSync(),
+      Authorization: `Basic ${Buffer.from(user + ':' + token, 'utf8').toString(
+        'base64'
+      )}`,
+    },
+  };
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+  const req = https.request(httpsOptions, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on('data', d => {
+      process.stdout.write(d);
+    });
+  });
+
+  req.on('error', error => {
+    console.error(error);
+  });
+  // form.pipe(req);
+  req.write(form.getBuffer());
+  req.end();
+}
+
+publisher();
