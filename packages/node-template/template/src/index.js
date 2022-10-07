bf14a9022408188a2bbfeb518399eb9fabcd3d1e
kdb621 <liradb2000@live.co.kr>
2022-10-07 13:55:38 +0900

    Update node template;

--- packages/node-template/template/src/index.js
+++ packages/node-template/template/src/index.js
@@ -1,18 +1,31 @@
-import { registerNodeType } from "LiteGraph";
 
-// Make node
-function TestNode() {
-  this.addInput("input", "*");
-  this.addOutput("output", 0);
+import LiteGraph from "litegraph.js";
+
+// This is the Fabrica™ Node Class Definition. 
+export class FirstNode {
+  static path = "my/folder";
+
+  static title = "FirstNode";
+  static description = "This is a my first node!";
+
+  constructor() {
+    this.addInput("input", "number");
+    this.addInput("eventInput", LiteGraph.EVENT);
+
+    this.addOutput("output", "number");
+    this.addOutput("eventOutput", LiteGraph.EVENT);
+
+    this.widget = this.addWidget("button", "click", null, function () {
+      console.log("Hello, Design-Express!");
+    });
+  }
+
+  onExecute() {
+    const inputData = this.getInputData(0);
+    this.setOutputData(0, inputData);
+  }
+
+  onAction(action, parameters) {
+    this.triggerSlot(1, parameters);
+  }
 }
-
-TestNode.prototype.onExecute = function () {
-  let input = this.getInputData(0);
-  this.setOutputData(0, input);
-};
-
-TestNode.title = "TestNode";
-TestNode.description = "This is the example.";
-
-// Expose Node
-registerNodeType("test/TestNode", TestNode);
