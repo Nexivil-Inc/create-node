554a1920d1d9be412c846c3d8d11c37f6ab31024
kdb621 <liradb2000@live.co.kr>
2023-09-05 01:50:25 +0900

    [node-sciprts] Fix runtime condition Regex;

--- packages/node-scripts/plugins/runtimeModder.js
+++ packages/node-scripts/plugins/runtimeModder.js
@@ -42,15 +42,18 @@ if(typeof ${RuntimeGlobals.require} !== "undefined") {
       console.error(e);
     }
 
-    const removeCondition = new RegExp(
-      /(\w\.f\.j=.+else if)(\([^)]+\))([^=]+=fetcher)/
+    const removeConditionOld = new RegExp(
+      /(\w\.f\.j=.+else if)(\(666!=t\))([^=]+=fetcher)/
+    );
+    const removeConditionNew = new RegExp(
+      /(\w\.f\.j=.+\.push\([^)]+\);)(else if\(\/\^.+\$\/\.test\([^)]+\)[^;]+;)(else[^=]+=fetcher)/
     );
     compiler.hooks.emit.tapAsync('ReplacePlugin', (compilation, callback) => {
       Object.keys(compilation.assets).forEach(filename => {
         if (filename === 'runtime.js') {
           let source = compilation.assets[filename].source();
-          if (!removeCondition.exec(source)[2].includes('666!='))
-            source = source.replace(removeCondition, '$1(true)$3');
+          source = source.replace(removeConditionOld, '$1(true)$3');
+          source = source.replace(removeConditionNew, '$1$3');
 
           compilation.assets[filename] = {
             source: () => source,
